151073c64d634e43705174964664dcac45311756
gediont <teshomegedion@gmail.com>
2020-09-02 14:27:01 +0300

    Updates

---
+++ precache-manifest.83cd074926642bff93fb0bd8decbfcc8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2080d5ffdea289ee0d34671e436535c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ca3384ad3e822e84bcb",
+    "url": "/static/css/main.aab7ef75.chunk.css"
+  },
+  {
+    "revision": "3cedcba4f9f093fde869",
+    "url": "/static/js/2.d86a98eb.chunk.js"
+  },
+  {
+    "revision": "b186e960a54ffa03b7384eadfa188126",
+    "url": "/static/js/2.d86a98eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca3384ad3e822e84bcb",
+    "url": "/static/js/main.be3fa630.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "8173b0184be856a1a20bbbd168b6b825",
+    "url": "/static/media/blog-post1.8173b018.md"
+  },
+  {
+    "revision": "dcc6b009d9de68490e1b22d6712e0724",
+    "url": "/static/media/blog-post2.dcc6b009.md"
+  }
+]);
