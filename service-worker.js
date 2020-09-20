d191c6aab64f014df25917dc30169ece2ecd003e
gediont <teshomegedion@gmail.com>
2020-09-20 17:43:59 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30661f51fc3799a9797a8caa44ce0a2a.js"
+  "/precache-manifest.c49e6ee1d978fe6be3ae53b0571c3e63.js"
 );
 
 self.addEventListener('message', (event) => {
--- static/media/blog-post2.dcc6b009.md
+++
@@ -1,5 +0,0 @@
-# Another blog post
-
-#### March 23, 2020 by [YoursTruly](/)
-
-Keep tuned in, coming soon.
